8c0f1353e83147bc4836b3a2be79db86b144ff33
caovanson2121231267312 <son1669063793@gmail.com>
2023-09-30 01:22:07 +0700

    u

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,11 @@ export default {
                 type: 'text/css',
                 href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
             },
+            {
+                rel: 'stylesheet',
+                type: 'text/css',
+                href: 'https://cdn.jsdelivr.net/npm/suneditor@latest/dist/css/suneditor.min.css',
+            },
         ],
     },
 
