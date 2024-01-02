076785a03b33213e94583273bac38372ce42e087
caovanson2121231267312 <son1669063793@gmail.com>
2024-01-02 22:45:43 +0700

    u

--- pages/admin/medias/_id/edit.vue
+++ pages/admin/medias/_id/edit.vue
@@ -23,6 +23,78 @@
                                 <b-form-checkbox v-model="status" switch size="lg"></b-form-checkbox>
                             </div>
                         </div>
+
+                        <hr class="support-hr" />
+
+                        <div class="mt-3 mb-6">
+                            <div class="mb-2">
+                                <strong class="service-title">
+
+                                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M2.22003 12.6735L2.2067 12.6868C2.0267 12.2935 1.91337 11.8468 1.8667 11.3535C1.91337 11.8402 2.04003 12.2802 2.22003 12.6735Z"
+                                            fill="#FFB761" />
+                                        <path
+                                            d="M6.5 6.91992C7.37629 6.91992 8.08666 6.20954 8.08666 5.33325C8.08666 4.45696 7.37629 3.74658 6.5 3.74658C5.6237 3.74658 4.91333 4.45696 4.91333 5.33325C4.91333 6.20954 5.6237 6.91992 6.5 6.91992Z"
+                                            fill="#FFB761" />
+                                        <path
+                                            d="M11.2934 1.3335H5.70671C3.28004 1.3335 1.83337 2.78016 1.83337 5.20683V10.7935C1.83337 11.5202 1.96004 12.1535 2.20671 12.6868C2.78004 13.9535 4.00671 14.6668 5.70671 14.6668H11.2934C13.72 14.6668 15.1667 13.2202 15.1667 10.7935V9.26683V5.20683C15.1667 2.78016 13.72 1.3335 11.2934 1.3335ZM14.08 8.3335C13.56 7.88683 12.72 7.88683 12.2 8.3335L9.42671 10.7135C8.90671 11.1602 8.06671 11.1602 7.54671 10.7135L7.32004 10.5268C6.84671 10.1135 6.09337 10.0735 5.56004 10.4335L3.06671 12.1068C2.92004 11.7335 2.83337 11.3002 2.83337 10.7935V5.20683C2.83337 3.32683 3.82671 2.3335 5.70671 2.3335H11.2934C13.1734 2.3335 14.1667 3.32683 14.1667 5.20683V8.40683L14.08 8.3335Z"
+                                            fill="#FFB761" />
+                                    </svg>
+                                    <span>Hình ảnh</span>
+                                </strong>
+                                <i class="img-note">(Tối thiểu không quá 2mb)</i>
+                            </div>
+
+                            <div class="input-grop">
+                                <div class="box-x d-flex justify-content-between p-2">
+                                    <div class="box-img">
+                                        <input type="file" hidden @change="handleFileChange" id="img" />
+                                        <img v-if="image" :src="image" alt="">
+                                        <img v-else src="@/static/images/teacher-training/Rectangle4052.png" alt="">
+                                        <input id="images" type="file" class="d-none" />
+                                    </div>
+                                    <div class="action">
+                                        <div @click="delete_img()" class="mb-3 btn-service-delete" v-b-tooltip.hover
+                                            title="Xoá ảnh">
+                                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="16" cy="16" r="16" fill="#F2F2F2" />
+                                                <g clip-path="url(#clip0_207_5736)">
+                                                    <path
+                                                        d="M16.0639 11.1534C18.1966 11.1534 20.3323 11.1594 22.4651 11.1475C22.7971 11.1475 22.9227 11.1802 22.8779 11.5789C22.6266 13.849 22.3634 16.1222 22.2108 18.4013C22.1241 19.6896 22.0194 20.9719 21.9147 22.2572C21.8549 22.9951 21.7921 23.4801 21.6604 24.2061C21.4899 25.1344 21.1071 26.0002 19.755 25.9883C17.2813 25.9913 14.8076 25.9645 12.3368 26.0002C11.3198 26.0151 10.5152 25.8038 10.2789 24.2061C10.0127 22.412 9.44433 14.1019 9.44433 14.1019L9.21699 11.7157C9.21699 11.7157 9.15418 11.1534 9.70755 11.1534C11.8283 11.1534 13.9461 11.1534 16.0669 11.1534H16.0639ZM16.5784 18.55C16.5784 17.2736 16.5784 15.9972 16.5784 14.7208C16.5784 14.2983 16.363 14.0305 16.028 14.0216C15.6541 14.0127 15.3849 14.3072 15.3849 14.7387C15.3849 17.2766 15.3849 19.8145 15.3879 22.3554C15.3879 22.8047 15.6361 23.1052 15.9981 23.1171C16.3361 23.129 16.5724 22.8107 16.5724 22.3376C16.5724 21.0761 16.5724 19.8115 16.5724 18.55H16.5784ZM19.4649 18.5411C19.4649 17.2498 19.4649 15.9556 19.4649 14.6643C19.4649 14.4977 19.4858 14.331 19.3153 14.212C19.1239 14.0781 18.9444 13.9591 18.6961 14.0692C18.3791 14.209 18.2834 14.453 18.2864 14.7803C18.2953 17.2944 18.2953 19.8115 18.3013 22.3257C18.3013 22.8017 18.5316 23.1082 18.8816 23.1112C19.2316 23.1171 19.4709 22.8017 19.4709 22.3376C19.4709 21.0731 19.4709 19.8086 19.4709 18.5441L19.4649 18.5411ZM13.6918 18.5708C13.6918 17.2528 13.6918 15.9347 13.6918 14.6167C13.6918 14.218 13.5213 14.0305 13.1594 14.0246C12.8453 14.0186 12.5911 14.2686 12.5911 14.6167C12.5881 17.2379 12.5881 19.8621 12.5911 22.4834C12.5911 22.8583 12.8663 23.1409 13.1833 23.1082C13.5513 23.0695 13.7038 22.8702 13.6978 22.4863C13.6799 21.1832 13.6918 19.877 13.6918 18.5738V18.5708Z"
+                                                        fill="#979797" />
+                                                    <path
+                                                        d="M15.9776 10.0553C13.7461 10.0553 11.5147 10.0553 9.28024 10.0553C8.58927 10.0553 8 9.51382 8 8.88008C8 8.29097 8.58628 7.72864 9.25631 7.72269C10.5844 7.71079 11.9125 7.70781 13.2406 7.72864C13.6085 7.73459 13.7222 7.63938 13.7042 7.26152C13.6713 6.59802 14.2786 6.01486 14.9576 6.00891C15.6366 6.00296 16.3156 6.04462 16.9916 5.99999C17.7035 5.95238 18.3436 6.6129 18.2868 7.23771C18.2509 7.63641 18.4124 7.73162 18.7893 7.72566C20.0755 7.70781 21.3617 7.74947 22.645 7.71674C23.5094 7.69591 24.3081 8.35048 23.8833 9.40076C23.7308 9.7816 23.312 10.0494 22.7616 10.0494C20.599 10.0494 18.4393 10.0494 16.2767 10.0494C16.178 10.0494 16.0793 10.0494 15.9806 10.0494L15.9776 10.0553Z"
+                                                        fill="#979797" />
+                                                </g>
+                                                <defs>
+                                                    <clipPath id="clip0_207_5736">
+                                                        <rect width="16" height="20" fill="white"
+                                                            transform="translate(8 6)" />
+                                                    </clipPath>
+                                                </defs>
+                                            </svg>
+                                        </div>
+                                        <label for="img" class="btn-service-upload d-block" v-b-tooltip.hover
+                                            title="Tải ảnh lên">
+                                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="16" cy="16" r="16" fill="#F2F2F2" />
+                                                <path d="M10 11.3846L16 6M16 6L22 11.3846M16 6L16 13" stroke="#979797"
+                                                    stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                                    d="M15 15V18.9375C15 19.4898 15.4477 19.9375 16 19.9375C16.5523 19.9375 17 19.4898 17 18.9375V15H23C24.1046 15 25 15.8954 25 17V22C25 23.1046 24.1046 24 23 24H9C7.89543 24 7 23.1046 7 22V17C7 15.8954 7.89543 15 9 15H15Z"
+                                                    fill="#979797" />
+                                            </svg>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
                         <div>
                             <b-form-group>
                                 <div class="label">
@@ -153,6 +225,8 @@ export default {
             tieu_de: '',
             status: true,
             link: '',
+            image: null,
+            file: null,
             // noi_dung: '',
         };
     },
@@ -169,6 +243,29 @@ export default {
         }
     },
     methods: {
+        delete_img() {
+            this.image = null;
+            this.file = null;
+        },
+        handleFileChange(event) {
+            const img = event.target.files[0];
+            this.file = img;
+            if (img) {
+                this.image = URL.createObjectURL(img);
+            }
+        },
+        add_benefit() {
+            console.log(this.name_benefit, this.link_benefit)
+            if (this.name_benefit == '' || this.link_benefit == '') {
+                toastr.error('Bạn cần nhập đầy đủ thông tin');
+                return
+            }
+            const newId = this.todos.length + 1;
+            this.todos.push({ id: newId, name_benefit: this.name_benefit, link_benefit: this.link_benefit });
+            this.name_benefit = ''
+            this.link_benefit = ''
+            this.$refs['my-modal'].hide()
+        },
         async load_type() {
             await api.get('tin-tuc/get-loai-tin-tuc', {
                 'Content-Type': 'multipart/form-data',
@@ -195,6 +292,7 @@ export default {
                 // this.selected = user?.vai_tro
                 this.selected = user?.type
                 this.link = user?.link
+                this.image = user?.anh_dai_dien
                 this.noi_dung = user?.noi_dung
 
                 this.suneditorInstance.setContents(user?.noi_dung);
@@ -210,6 +308,7 @@ export default {
             formData.append('type', this.selected)
             formData.append('noi_dung', this.noi_dung)
             formData.append('link', this.link)
+            formData.append('image', this.file)
 
             await api.post('tin-tuc/sua', formData, {
                 'Content-Type': 'multipart/form-data',
@@ -295,6 +394,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.box-x {
+    overflow: hidden;
+    border-radius: 10px;
+    border: 1px solid #E5E5E5;
+    background: #FFF;
+
+    .box-img {
+        overflow: hidden;
+        height: 109px;
+        border-radius: 10px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+}
+
 .medias {
     i {
         color: #2D2D2D;
