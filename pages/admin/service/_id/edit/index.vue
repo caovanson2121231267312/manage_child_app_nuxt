bb5ee10401c8664966e432daf1b68e1b7309ed7b
caovanson2121231267312 <71201798+caovanson2121231267312@users.noreply.github.com>
2024-03-27 11:00:13 +0700

    Abc

--- pages/admin/service/_id/edit/index.vue
+++ pages/admin/service/_id/edit/index.vue
@@ -370,7 +370,7 @@
                                     <div class="mb-3">
                                         <div class="d-flex justify-content-between align-items-center">
                                             <title-header>
-                                                đường dẫn hợp đồng <span class="text-danger">*</span>
+                                                Đường dẫn hợp đồng <span class="text-danger">*</span>
                                             </title-header>
                                         </div>
                                     </div>
