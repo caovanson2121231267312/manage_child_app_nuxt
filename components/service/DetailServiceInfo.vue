d9911b35d456a4b8f39136e101f69cdc08eccff5
caovanson2121231267312 <71201798+caovanson2121231267312@users.noreply.github.com>
2024-08-21 11:19:32 +0700

    u

--- components/service/DetailServiceInfo.vue
+++ components/service/DetailServiceInfo.vue
@@ -165,7 +165,7 @@
                                 </a>
                             </div>
                             <div>
-                                <a class="" :href="'sms:+' + data?.phuHuynh?.dien_thoai + '?body=Xin chào'"></a>
+                                <a class="" :href="'sms:+' + data?.phuHuynh?.dien_thoai + '?body=Xin chào'">
                                 <svg width="38" height="38" viewBox="0 0 38 38" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="19" cy="19" r="19" fill="#F2F2F2" />
