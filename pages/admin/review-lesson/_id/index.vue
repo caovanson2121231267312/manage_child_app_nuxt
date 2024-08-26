91cc35c16e1d457ae886e33ce1915b7f582349ba
caovanson2121231267312 <71201798+caovanson2121231267312@users.noreply.github.com>
2024-08-26 03:42:24 -0700

    u

--- pages/admin/review-lesson/_id/index.vue
+++ pages/admin/review-lesson/_id/index.vue
@@ -219,8 +219,8 @@
                                             <input type="radio"  
                                             :name="'radio-size' + k" 
                                             :value="h" 
-                                            :checked="h === i.muc_do_da_cho"  
-                                            class="custom-control-input" 
+                                            :checked="h == i.muc_do_da_cho"  
+                                       
                                             :id="'__BVID__' + k">
                                             <span class="answer">{{ h }}</span>
                                         <!-- </b-form-radio> -->
